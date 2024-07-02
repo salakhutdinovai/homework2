7f1a3d9682894a4de81403147ad73cc1a5ca4ef9
salakhutdinovai <144042545+salakhutdinovai@users.noreply.github.com>
2024-07-02 19:55:09 +0300

    1

---
+++ chat/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  chat
+//
+//  Created by Айнур Салахутдинов on 01.07.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
